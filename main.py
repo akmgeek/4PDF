2c15f2fcc724ef14d5aed472bc9b40d7ec4d2ea5
Amit-Maurya <akmgeek@gmail.com>
2022-09-28 00:33:24 +0530

    Merge functionality done

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 from PyQt5.QtWidgets import QMainWindow,QApplication, QFileDialog, QTableWidgetItem, QHeaderView, QPushButton
+from PyPDF2 import PdfMerger
 
 class main_ui(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(main_ui,self).__init__()
         self.setupUi(self)
         self.count = 0
+        self.pdfs = []
         header = self.tableWidget.horizontalHeader()       
         header.setSectionResizeMode(0, QHeaderView.Stretch)
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
@@ -19,25 +21,27 @@ class main_ui(QMainWindow, Ui_MainWindow):
 
         
         self.add_file_bttn.clicked.connect(self.add_pdf_file)
+        self.pushButton_7.clicked.connect(self.on_merge_start)
 
     def add_pdf_file(self):
         filename, _ = QFileDialog.getOpenFileName(None, filter="PDF (*.pdf)")
         if not filename:
             print("please select the .pdf file")
-        # print(len(file_name), " ", str(filename))
+
         print(filename.split("/"))
         file_name = filename.split("/")
-        # file_name = len(file_name)-1
         file_name = file_name[len(file_name)-1]
         print(file_name)
         
         self.create_bttns()    
         self.tableWidget.insertRow(self.count)
-        self.tableWidget.setItem(self.count, 0, QTableWidgetItem(str(filename)))
+        self.tableWidget.setItem(self.count, 0, QTableWidgetItem(str(file_name)))
         self.tableWidget.setCellWidget(self.count, 1, self.view_bttn)
         self.tableWidget.setCellWidget(self.count, 2, self.delete_bttn)
         self.create_webview(filename)
         self.count = +1
+        self.pdfs.append(filename)
+        print(self.pdfs)
 
     def create_webview(self,filename):
         # view = QWebEngineView()
@@ -50,10 +54,17 @@ class main_ui(QMainWindow, Ui_MainWindow):
         
     def create_bttns(self):
         self.view_bttn = QPushButton()
-        self.view_bttn.setIcon(QIcon('icons/visual.png'))
+        self.view_bttn.setIcon(QIcon('icons/eye.png'))
         self.delete_bttn = QPushButton()
         self.delete_bttn.setIcon(QIcon('icons/delete.png'))
-        
+    
+    def on_merge_start(self):        
+        merger = PdfMerger()
+        for pdf in self.pdfs:
+            merger.append(pdf)
+        merger.write("result.pdf")
+        merger.close()
+
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
